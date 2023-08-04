3d637bf7cdc108f43c0c1f6e8b698752486668a0
Reksee <ramonfernando@id.uff.br>
2023-08-04 18:47:08 -0300

    Exercise 02: Done!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(simpleText, numberOfRepeats) {
+    let i; 
+    let concatString = ""; 
+    if (numberOfRepeats >= 0) {
+        for (let i = 0; i < numberOfRepeats; i++) {
+            concatString = concatString.concat(simpleText);
+        }
+        return concatString;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
