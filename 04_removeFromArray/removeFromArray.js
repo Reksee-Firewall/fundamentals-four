24579a0e9d49adeb046002dc8be1ebd792741b85
Reksee <ramonfernando@id.uff.br>
2023-08-04 20:38:44 -0300

    Exercise 04: Done!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(initArray, ...Args) {
+    let arrayLength = initArray.length; 
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = 0; j < arrayLength; j++) {
+            if (arguments[i] === initArray[j]) {
+                initArray.splice(j, 1);
+            }
+        }
+    }
+    return (initArray); 
 };
 
 // Do not edit below this line
