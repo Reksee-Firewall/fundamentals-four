4c8b1391921c5d97ea675cc76fd8aaff230ed7e2
Reksee <ramonfernando@id.uff.br>
2023-08-04 21:01:33 -0300

    Exercise 05: Done!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sumNumbers = 0; 
+    let bigNum, smallNum; 
 
+    if ((numOne < 0) || (numTwo < 0) || 
+        (typeof numOne !== 'number') || 
+        (typeof numTwo !== 'number')) {
+        return 'ERROR'; 
+    }
+    if (numOne > numTwo) {
+        bigNum = numOne; 
+        smallNum = numTwo;
+    } else {
+        bigNum = numTwo;
+        smallNum = numOne;
+    }
+    for (let i = 0; i < (bigNum - smallNum + 1); i++) {
+        sumNumbers += smallNum + i;
+    }
+    return sumNumbers; 
 };
 
 // Do not edit below this line
