2d8b675a9003ebe0ccb5d94059717e3f8a7f0156
Reksee <ramonfernando@id.uff.br>
2023-08-05 01:01:08 -0300

    Exercise 12: Done!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    const arrayLength = peopleArray.length,
+        currentDate = new Date(), 
+        currentYear = currentDate.getFullYear();
+    let ageDiff = 0, 
+        age = 0, 
+        higherAge = 0;
+        olderIndex = 0;
+    for (let i = 0; i < arrayLength; i++) {
+        if (peopleArray[i].yearOfDeath === undefined)
+            age = currentYear - peopleArray[i].yearOfBirth;
+        else 
+            age = peopleArray[i].yearOfDeath - peopleArray[i].yearOfBirth; 
+        ageDiff = higherAge - age; 
+        if (ageDiff < 0) {
+            higherAge = age;
+            olderIndex = i;
+        }
+    }
+    return (peopleArray[olderIndex]); 
 };
 
 // Do not edit below this line
