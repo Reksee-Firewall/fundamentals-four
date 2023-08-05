eecd2a5753846a4b060c6c742fae38cbaaa3db81
Reksee <ramonfernando@id.uff.br>
2023-08-04 22:54:40 -0300

    Exercise 08: Done!

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return (parseInt(numOne, 10) + parseInt(numTwo, 10));
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+	return (parseInt(numOne, 10) - parseInt(numTwo, 10));
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+  let sum = 0;
+  for (let i = 0; i < sumArray.length; i++) {
+    sum += parseInt(sumArray[i], 10); 
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(multArray) {
+  let mult = 1; 
+  for (let i = 0; i < multArray.length; i++) {
+    mult *= parseInt(multArray[i], 10); 
+  }
+  return mult;
 };
 
-const power = function() {
-	
+const power = function(numOne, toPower) {
+	return Math.pow(numOne, toPower); 
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let fact = 1;
+  for (let i = 0; i < number; i++) {
+    fact *= (number - i);
+  }
+  return fact; 
 };
 
 // Do not edit below this line
