e12fd7054aef0d8606768f2fa00880fef7a5f87d
Reksee <ramonfernando@id.uff.br>
2023-08-04 22:28:42 -0300

    Exercise 07: Done!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  // Tc = 5/9(Tf -32)
+  let celsiusTemp = ((5/9) * (fahrenheitTemp - 32)).toFixed(1);
+  return parseFloat(celsiusTemp, 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  // Tf = 9/5(Tc) + 32
+  let fahrenheitTemp = ((9/5) * (celsiusTemp) + 32).toFixed(1);
+  return parseFloat(fahrenheitTemp, 10);
 };
 
 // Do not edit below this line
