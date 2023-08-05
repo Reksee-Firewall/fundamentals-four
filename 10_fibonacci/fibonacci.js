9a05826c7fbd8aae3659adf4415e53130ab3490c
Reksee <ramonfernando@id.uff.br>
2023-08-05 00:20:16 -0300

    Exercise 10: Done!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberPosition) {
+    if (numberPosition < 0)   
+        return "OOPS";
+    let numOne = 1;
+    let numTwo = 1;
+    let temp = 0; 
+    for(let i = 0; i < (numberPosition - 2); i++) {
+        numOne = numOne + numTwo; 
+        temp = numOne; 
+        numOne = numTwo; 
+        numTwo = temp; 
+    }
+    return numTwo;
 };
 
 // Do not edit below this line
